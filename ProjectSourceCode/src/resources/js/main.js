7874ae15512148e67f320b61fd1894b004d46b24
Maggie Schneider <marg.schneider4@gmail.com>
2025-10-30 13:26:37 -0600

    working on prompts page, and making it easier to test front end with path modification

--- ProjectSourceCode/src/resources/js/main.js
+++ ProjectSourceCode/src/resources/js/main.js
@@ -7,3 +7,18 @@ document.querySelectorAll(".nav-pill").forEach(link => {
     link.classList.add("active");
   }
 });
+
+//  only run on prompts page to set today's date
+document.addEventListener("DOMContentLoaded", () => {
+  const dateElement = document.getElementById("date");
+  if (dateElement) {
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    dateElement.textContent = formattedDate;
+  }
+});
