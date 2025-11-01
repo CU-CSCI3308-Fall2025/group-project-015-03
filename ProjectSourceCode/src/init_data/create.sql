6022cdbdfb226b5931576b3b9736eda43395b7e8
summer smith <marcysmith792@gmail.com>
2025-11-01 16:34:41 -0600

    initial databse setup

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS prompts CASCADE;
+CREATE TABLE IF NOT EXISTS prompts (
+    prompt_id INT PRIMARY KEY SERIAL,
+    prompt_txt VARCHAR(50);
+);
+
+DROP TABLE IF EXISTS responses CASCADE;
+CREATE TABLE IF NOT EXISTS responses (
+    response_id INT PRIMARY KEY SERIAL,
+    response_txt VARCHAR(500),
+    FOREIGN KEY (user) references users(username)  
+);
+
+DROP TABLE IF EXISTS responses_to_users CASCADE;
+CREATE TABLE IF NOT EXISTS response_to_users (
+    username VARCHAR(20) PRIMARY KEY,
+    response_id INT NOT NULL
+);
