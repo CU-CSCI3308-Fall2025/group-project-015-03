7c1220433495b10fc183756aaaa3c63d45ceb77a
Jackson Erb <jaer4783@colorado.edu>
2025-11-04 12:54:45 -0700

    Added handlebars and edited index.js to reroute using handlebars layouts

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -6,6 +6,12 @@ const path = require('path');
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
 
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname + '/src/views/layouts'),
+  partialsDir: path.join(__dirname + '/src/views/partials'),
+});
+
 // database configuration
 const dbConfig = {
   host: 'db', // the database server 
@@ -25,27 +31,39 @@ db.connect()
   })
   .catch(error => {
     console.log('ERROR:', error.message || error);
-  });
+});
 
 // middleware
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/src/views'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // serve static files (for your frontend)
 app.use(express.static(path.join(__dirname, 'src/resources')));
-app.use(express.static(path.join(__dirname, 'src/views')));
+
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
 
 app.get('/register', (req, res) => {
-    //TODO
+    res.render('pages/index', { title: 'Register'});
 });
 
 app.get('/login', (req, res) => {
-    //TODO
+    res.render('pages/index', { title: 'Login'})
 });
 
 app.get('/home', (req, res) => {
-    //TODO
+    res.render('pages/feed', { title: 'Home'});
 });
+
+app.get('/profile', (req, res) => {
+    res.render('pages/profile', { layout: 'secondary' , title: 'Profile' })
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
