7490da02dfabc329641353384cddb884322f8db7
Maggie Schneider <marg.schneider4@gmail.com>
2025-11-06 15:42:20 -0700

    first part of lab 10

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -99,7 +99,8 @@ app.post('/journal/new', (req, res) => {
   res.redirect('/journal');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
