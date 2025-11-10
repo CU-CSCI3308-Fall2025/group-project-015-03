a3af7c2834e0dd48bb3481d7f3d00ec6d89623e8
Jackson Erb <jaer4783@colorado.edu>
2025-11-10 14:46:20 -0700

    Added login and register functionality

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,23 +1,12 @@
-INSERT INTO users(
-    username,
-    password
-) VALUES 
-(
-    "marcylake", "tumblestackadmin"
-),
-(
-    "keiko", "purpledragon"
-);
-
 INSERT INTO prompts(
     prompt_txt
 )
 VALUES (
-    "What is your favorite color? Why?"
+    'What is your favorite color? Why?'
 ),
 (
-    "What is the velocity of an unladen European sparrow?"
+    'What is the velocity of an unladen European sparrow?'
 ),
 (
-    "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+    'How much wood could a woodchuck chuck if a woodchuck could chuck wood?'
 );
