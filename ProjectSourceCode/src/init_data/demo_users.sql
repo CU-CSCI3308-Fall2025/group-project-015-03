2c90523e59dce37d9b12c5f8e6f0784b922d2583
Maya Hernandez <mahe4561@colorado.edu>
2025-12-02 16:51:14 -0600

    demo users database

---
+++ ProjectSourceCode/src/init_data/demo_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, pronouns, nickname, quote, pfp_link, theme)
+VALUES
+    ('Holly', 'password123', 'she/her', 'Demi', 'Hello world!', 'https://example.com/pfp1.jpg', 'pink'),
+    ('Jack', 'password123', 'he/him', 'Dax', 'Keep going!', 'https://example.com/pfp2.jpg', 'blue'),
+    ('Max', 'password123', 'they/them', 'Sky', 'Be yourself', 'https://example.com/pfp3.jpg', 'green'),
+    ('Luna', 'password123', 'she/they', 'Lu', 'Dream big, shine bright.', 'https://example.com/pfp11.jpg', 'silver'),
+    ('Finn', 'password123', 'he/him', 'Finny', 'Stay curious.', 'https://example.com/pfp12.jpg', 'blue');
